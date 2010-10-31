a607817c0efe6d4cdd325748d62c672ca438a99c
Fraser Hess <fraser@sweeterrhythm.com>
2010-10-31 15:23:44 -0600

    Change name of receipt file to default name

--- ValidateStoreReceipt.m
+++ ValidateStoreReceipt.m
@@ -6,7 +6,7 @@ int main (int argc, const char * argv[]) {
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
 
 	// put the example receipt on the desktop (or change that path)
-	if (!validateReceiptAtPath(@"~/Desktop/receipt.cer"))
+	if (!validateReceiptAtPath(@"~/Desktop/receipt"))
 		exit(173);
 	
     // insert code here...
